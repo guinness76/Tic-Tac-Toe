0df0edad976216cadd5f6bd41cdd1583a84aaeec
Patrick Gross <bears_lair@protonmail.com>
2024-10-28 19:50:41 -0700

    working on displaying the board to the console

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Objects:
-//     Player: name, icon(X or O)
-//     Gameboard: all nine positions by column and row
-//     DisplaytoPlayers
-
+// Ask for the names of both players
 const playerOne = prompt("Name of Player for X");
 const playerTwo = prompt("Name of Player for O");
 
+// Object factory function to create players (assign name, icon, and list of moves)
 function players(playerOne, playerTwo) {
   const playerOneName = playerOne;
   const playerOneIcon = "X";
@@ -21,29 +18,51 @@ function players(playerOne, playerTwo) {
   ];
 }
 
+// Players created with factory function and assigned to currentPlayers variable
 const currentPlayers = players(playerOne, playerTwo);
 
-// Gameboard array listing each rowColumn
-gameboard = [
-  { oneOne: " " },
-  { oneTwo: " " },
-  { oneThree: " " },
-  { twoOne: " " },
-  { twoTwo: " " },
-  { twoThree: " " },
-  { threeOne: " " },
-  { threeTwo: " " },
-  { threeThree: " " },
-];
+let matchWin = false;
 
-// Win conditions array listing rowColumn
-winConditions = [
-  [oneOne, oneTwo, oneThree],
-  [twoOne, twoTwo, twoThree],
-  [threeOne, threeTwo, threeThree],
-  [oneOne, twoOne, threeOne],
-  [twoOne, twoTwo, twoThree],
-  [threeOne, threeTwo, threeThree],
-  [oneOne, twoTwo, threeThree],
-  [oneThree, twoTwo, threeOne],
-];
+let oneOne = " ";
+let oneTwo = " ";
+let oneThree = " ";
+let twoOne = " ";
+let twoTwo = " ";
+let twoThree = " ";
+let threeOne = " ";
+let threeTwo = " ";
+let threeThree = " ";
+
+function displayGame() {
+  console.log(`[${oneOne}][${oneTwo}][${oneThree}]`);
+  console.log(`[${twoOne}][${twoTwo}][${twoThree}]`);
+  console.log(`[${threeOne}][${threeTwo}][${threeThree}]`);
+}
+
+displayGame();
+
+//Loops game until a player wins
+while ((matchWin = false)) {
+  if (
+    currentPlayers[0].playerOneMoves == [] &&
+    currentPlayers[1].playerTwoMoves == []
+  ) {
+    let playerOneTurn = true;
+  }
+
+  if (playerOneTurn === true) {
+  } else {
+  }
+
+  // Win conditions array listing rowColumn
+  winConditions = [
+    [oneOne, oneTwo, oneThree],
+    [twoOne, twoTwo, twoThree],
+    [threeOne, threeTwo, threeThree],
+    [oneOne, twoOne, threeOne],
+    [twoOne, twoTwo, twoThree],
+    [threeOne, threeTwo, threeThree],
+    [oneOne, twoTwo, threeThree],
+    [oneThree, twoTwo, threeOne],
+  ];
+}
